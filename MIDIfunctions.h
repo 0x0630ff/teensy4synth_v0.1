90c1afa5e24dec46096b023efed684ce33c2b5ed
Geoff Llewellyn <geoffllewellyn@gmail.com>
2020-03-20 22:31:55 -0700

    var assignments

--- MIDIfunctions.h
+++ MIDIfunctions.h
@@ -3,6 +3,9 @@
 const byte BUFFER = 8;  // size of keyboard buffer
 const float DIV127 = (1.0 / 127.0);
 
+int globalNote;
+int globalVelocity;
+
 void MIDIContolChange(byte channel, byte control, byte value) {
     // Midi Switch
     switch (control)
@@ -24,18 +27,42 @@ void keyBuff(byte note, bool playNote) {
     static byte buff[BUFFER];
     static byte buffSize = 0;
     // Add note
+    if (playNote == true && (buffSize < BUFFER) ) {
+        // make oscilatore make noise...
+        buff[buffSize] = note;
+        buffSize++;
+        return;
+    }
+    // remove a note
+    else if (playNote == false && buffSize != 0) {
+        // more...
+    }
 }
 
 void midiNoteOn(byte channel, byte note, byte velocity) {
     // do something with the note, channel, velocity etc...
-    keyBuff(note, true);
+    // code here...
+    if ( 23 < note < 108) {
+        globalNote = note;
+        globalVelocity = velocity;
+        keyBuff(note, true);
+    }
 }
 
 void midiNoteOff(byte channel, byte note, byte velocity) {
+    // stop something with the note, channel, velocity etc...
     keyBuff(note, false);
+    // code here...
 }
 
 void MIDIsetup(void) {
     // set up midi via usb. call in setup();
     usbMIDI.setHandleControlChange(MIDIContolChange);
+    usbMIDI.setHandleNoteOff(midiNoteOff);
+    usbMIDI.setHandleNoteOn(midiNoteOn);
+
+    //DIN MIDI INPUT NEXT...
+    // MIDI.setHandleControlChange(MIDIContolChange);
+    // MIDI.setHandleNoteOn(midiNoteOn);
+    // MIDI.setHandleNoteOff(midiNoteOff);
 }
